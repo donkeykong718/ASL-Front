22c87b344eb715bf97fa87a8d167991e1dbec92e
richardsaudek <richard.saudek@gmail.com>
2023-04-04 18:40:45 -0400

    login box with sunglassses

--- asl-front/pages/login/index.jsx
+++ asl-front/pages/login/index.jsx
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect } from 'react';
-import '98.css';
-import '../../src/app/globals.css';
+import React, { useRef, useEffect } from "react";
+import "98.css";
+import "../../src/app/globals.css";
 
 export default function Login() {
   const windowRef = useRef(null);
@@ -39,14 +39,14 @@ export default function Login() {
         isDragging = false;
       };
 
-      windowElement.addEventListener('mousedown', handleMouseDown);
-      windowElement.addEventListener('mousemove', handleMouseMove);
-      windowElement.addEventListener('mouseup', handleMouseUp);
+      windowElement.addEventListener("mousedown", handleMouseDown);
+      windowElement.addEventListener("mousemove", handleMouseMove);
+      windowElement.addEventListener("mouseup", handleMouseUp);
 
       return () => {
-        windowElement.removeEventListener('mousedown', handleMouseDown);
-        windowElement.removeEventListener('mousemove', handleMouseMove);
-        windowElement.removeEventListener('mouseup', handleMouseUp);
+        windowElement.removeEventListener("mousedown", handleMouseDown);
+        windowElement.removeEventListener("mousemove", handleMouseMove);
+        windowElement.removeEventListener("mouseup", handleMouseUp);
       };
     }
   }, []);
@@ -56,11 +56,40 @@ export default function Login() {
       <div style={{ height: "100px" }}></div>
 
       <div className="flex-container">
-      <div className="window" style={{ height: "600px", width: "300px", backgroundColor: 'silver', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} ref={windowRef}>
-
-        {/* <div className="window" style={{ height: "600px", width: "300px" }} ref={windowRef}> */}
+        <div
+          className="window"
+          style={{
+            height: "600px",
+            width: "300px",
+            backgroundColor: "silver",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+          ref={windowRef}
+        >
           <div className="title-bar">
-            <div className="title-bar-text">Sign On</div>
+          <div
+              className="title-bar-icon"
+              style={{
+                height: "10%",
+                width: "10%",
+                // backgroundColor: "#005BAB",
+                // opacity: 1,
+              }}
+            >
+              <img
+                src="/aol-man.png"
+                alt=""
+                style={{ margin: "0 auto" }}
+              />
+            </div>
+            <div className="title-bar-text"
+              style={{
+              width:"180px"
+            }}
+            >Sign On</div>
             <div className="title-bar-controls">
               <button className="controls" aria-label="Minimize"></button>
               <button className="controls" aria-label="Maximize"></button>
@@ -68,17 +97,52 @@ export default function Login() {
             </div>
           </div>
           <div className="window-body">
-            <div className="aol-box" style={{ height: '100%', width: '100%', backgroundColor: '#005BAB', opacity: 1 }}>
-              <img src='/login-header.png' alt="" style={{ margin: '0 auto' }}/>
+            <div
+              className="aol-box"
+              style={{
+                height: "100%",
+                width: "100%",
+                backgroundColor: "#005BAB",
+                opacity: 1,
+                
+              }}
+            >
+           <img
+  src="/cool-man.png"
+  alt=""
+  style={{ margin: "0 auto", height: "40%", width: "40%", float: "right" }}
+/>
+
+              <div
+                className="ASL-title"
+                style={{
+                  height: "25%",
+                  width: "100%",
+                  backgroundColor: "#005BAB",
+                  opacity: 1,
+
+                }}>
+                 
+              <img
+                src="/ASL-logo-text.png"
+                alt=""
+                style={{ margin: "0 auto" }}
+              />
+              </div>
+              {/* <div style={{ height: "100px" }}></div> */}
+
             </div>
-            <div className="field-row-stacked" style={{ width: "200px", justifyContent: 'center' }}>
+            <div
+              className="field-row-stacked"
+              style={{ width: "200px", justifyContent: "center" }}
+            >
               <label for="text22">Screen Name</label>
               <input id="text22" type="user" value="" />
             </div>
             <div className="field-row-stacked" style={{ width: "200px" }}>
               <label for="text23">Password</label>
-            <input id="text23" type="password" value="" />
-            <button className="submit"></button>
+              <input id="text23" type="password" value="" />
+              <button className="submit"></button>
             </div>
           </div>
         </div>
