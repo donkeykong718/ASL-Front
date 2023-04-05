0219a5b9e2d1c5845ede757cc65bbbc86b79fdd2
Benson Lee <BensonL1334@gmail.com>
2023-04-04 22:56:05 -0400

    Basic homepage functions

--- asl-front/pages/homePage/index.jsx
+++ asl-front/pages/homePage/index.jsx
@@ -1,102 +1,204 @@
-// import React, { useMemo, useRef, useState } from 'react'
-
-// export default function homePage() {
-//   const [items, setItems] = useState([])
-//   const [query, setQuery] = useState('')
-//   const inputRef = useRef()
-
-//   const filteredItems = useMemo(() => {
-//     return items.filter(item => (
-//       item.toLowerCase().includes(query.toLowerCase())
-//     ))
-//   }, [items, query])
-
-//   function onSubmit(e) {
-//     e.preventDefault()
-//     const value = inputRef.current.value
-//     if (value === "") return
-//     setItems(prev =>
-//       [...prev, value])
-//     inputRef.current.value = ""
-//   }
-
-//   return (
-//     <>
-//       <h3>Search:</h3>
-//       <input
-//         value={query}
-//         onChange={e => setQuery(e.target.value)}
-//         placeholder="Search"
-//         type="search" />
-//       <br />
-//       <br />
-//       <form onSubmit={onSubmit}>
-//         <h3>New Items:</h3>
-//         <input ref={inputRef} type="text" />
-//         <button type="submit">Add</button>
-//       </form>
-//       <h3>Items:</h3>
-//       {filteredItems.map(item => (
-//         <div>{item}</div>
-//       ))}
-//     </>
-//   )
-// }
-
 import React, { useState, useEffect, useRef } from "react";
 
 
 const SearchbarDropdown = (props) => {
-  const { options, onInputChange } = props;
+  const { options, onInputChange, categoryChoice, setCategoryChoice } = props;
   const ulRef = useRef()
   const inputRef = useRef()
+  const buttonRef = useRef([])
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const value = inputRef.current.value
+    if (value) {
+      setCategoryChoice(defaultOptions.filter(option =>
+        option.toLowerCase().includes(value.toLowerCase())))
+    }
+  }
+
+  const onClick = (e, value) => {
+    setCategoryChoice([value])
+  }
+
+  useEffect(() => {
+    console.log(categoryChoice);
+  }, [categoryChoice])
 
   useEffect(() => {
     inputRef.current.addEventListener('click', e => {
       e.stopPropagation();
-      ulRef.current.style.display = "flex"
+      if (ulRef.current) {
+        ulRef.current.style.display = "flex"
+      }
     })
     document.addEventListener('click', e => {
-      ulRef.current.style.display = "none"
+      if (ulRef.current) {
+        ulRef.current.style.display = "none"
+      }
     })
   }, []);
 
+
   return (
     <div className="search-bar-dropdown">
-      <input
-        className="search-bar-field"
-        type="search"
-        placeholder="Search"
-        ref={inputRef}
-        onChange={onInputChange}
-      />
-      <ul className="search-bar-list" ref={ulRef}>
-        {options.map((option, index) => {
-          return (
-            <button
-              className="search-bar-list-item"
-              type="button"
-              key={index}
-            >
-              {option}
-            </button>
-          )
-        })}
-      </ul>
+      <form onSubmit={onSubmit}>
+        <input
+          className="search-bar-field"
+          type="search"
+          placeholder="Search"
+          ref={inputRef}
+          onChange={onInputChange}
+        />
+        <ul className="search-bar-list" ref={ulRef}>
+          {options.map((option, index) => {
+            buttonRef.current[index] = React.createRef()
+            return (
+              <button
+                className="search-bar-list-item"
+                type="submit"
+                key={index}
+                onClick={(e) => onClick(e, option)}
+                ref={buttonRef.current[index]}
+              >
+                {option}
+              </button>
+            )
+          })}
+        </ul>
+      </form>
     </div>
   )
 }
 
 // Simulated db
-const defaultOptions = [];
-for (let i = 0; i < 10; i++) {
-  defaultOptions.push(`Aegon ${i}`);
-  defaultOptions.push(`Daeron ${i}`);
-  defaultOptions.push(`Viserys ${i}`);
+const defaultOptions =
+  [
+    'Targaryen', 'Lannister', 'Stark', 'Baratheon', 'Tyrell',
+  ];
+
+const roomList =
+  [
+    'Aemon', 'Rhaegar', 'Daenerys',
+    'Tyrion', 'Jaime', 'Cersei',
+    'Jon', 'Robb', 'Sansa',
+    'Robert', 'Stannis', 'Renly',
+    'Margaery', 'Loras', 'Olenna',
+  ]
+
+
+// Category
+
+
+const CategoryScroll = (props) => {
+  const { categoryChoice, categoryOption, setCategoryOption } = props;
+  const onClick = (e) => {
+    const value = e.target.value;
+    setCategoryOption(value)
+  }
+
+  useEffect(() => {
+    // console.log(categoryOption);
+  }, [categoryOption])
+
+  const categories = categoryChoice.length > 0 ? categoryChoice : defaultOptions
+
+  return (
+    <div className="category-scroll-display">
+      {categories.map(choice => {
+        return (
+          <button
+            className="category-scroll-option"
+            onClick={onClick}
+            value={choice}
+            type="button"
+          >
+            {choice}
+          </button>
+        )
+      })}
+    </div>
+  )
 }
+
+// Room List
+
+
+
+const RoomList = (props) => {
+  const { categoryOption } = props;
+
+  if (categoryOption === "") {
+    return (
+      <div className="room-list-display">
+        <button type="button" className="Targaryen">Aemon</button>
+        <button type="button" className="Targaryen">Rhaegar</button>
+        <button type="button" className="Targaryen">Daenerys</button>
+        <button type="button" className="Lannister">Tyrion</button>
+        <button type="button" className="Lannister">Jaime</button>
+        <button type="button" className="Lannister">Cersei</button>
+        <button type="button" className="Stark">Jon</button>
+        <button type="button" className="Stark">Robb</button>
+        <button type="button" className="Stark">Sansa</button>
+        <button type="button" className="Baratheon">Robert</button>
+        <button type="button" className="Baratheon">Stannis</button>
+        <button type="button" className="Baratheon">Renly</button>
+        <button type="button" className="Tyrell">Margaery</button>
+        <button type="button" className="Tyrell">Loras</button>
+        <button type="button" className="Tyrell">Olenna</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="room-list-display">
+        {categoryOption === "Targaryen" && (
+          <>
+            <button type="button" className="Targaryen">Aemon</button>
+            <button type="button" className="Targaryen">Rhaegar</button>
+            <button type="button" className="Targaryen">Daenerys</button>
+          </>
+        )}
+        {categoryOption === "Lannister" && (
+          <>
+            <button type="button" className="Lannister">Tyrion</button>
+            <button type="button" className="Lannister">Jaime</button>
+            <button type="button" className="Lannister">Cersei</button>
+          </>
+        )}
+        {categoryOption === "Stark" && (
+          <>
+            <button type="button" className="Stark">Jon</button>
+            <button type="button" className="Stark">Robb</button>
+            <button type="button" className="Stark">Sansa</button>
+          </>
+        )}
+        {categoryOption === "Baratheon" && (
+          <>
+            <button type="button" className="Baratheon">Robert</button>
+            <button type="button" className="Baratheon">Stannis</button>
+            <button type="button" className="Baratheon">Renly</button>
+          </>
+        )}
+        {categoryOption === "Tyrell" && (
+          <>
+            <button type="button" className="Tyrell">Margaery</button>
+            <button type="button" className="Tyrell">Loras</button>
+            <button type="button" className="Tyrell">Olenna</button>
+          </>
+        )}
+      </div>
+    )
+  }
+};
+
+
+
+
+//Homepage component
 
 export default function Homepage() {
   const [options, setOptions] = useState([])
+  const [categoryChoice, setCategoryChoice] = useState("")
+  const [categoryOption, setCategoryOption] = useState("")
 
   const onInputChange = (e) => {
     const value = e.target.value
@@ -112,8 +214,16 @@ export default function Homepage() {
       <SearchbarDropdown
         options={options}
         onInputChange={onInputChange}
+        categoryChoice={categoryChoice}
+        setCategoryChoice={setCategoryChoice}
       />
       <button className="home-search-button">Search</button>
+      <CategoryScroll
+        categoryOption={categoryOption}
+        setCategoryOption={setCategoryOption}
+        categoryChoice={categoryChoice}
+      />
+      <RoomList categoryOption={categoryOption} />
     </div>
   )
 }
