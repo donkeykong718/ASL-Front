81292e540f0d77413827868f822c11338b7383ea
donkeykong718 <122040169+donkeykong718@users.noreply.github.com>
2023-04-06 14:56:00 -0400

    Actually fixes Vercel issue

--- asl-front/src/app/api/services/apiConfig.js
+++ asl-front/src/app/api/services/apiConfig.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 
-let apiUrl;
+const apiUrl = "https://asl-back.herokuapp.com"
 
-const apiUrls = {
-  production: "https://asl-back.herokuapp.com",
-  development: "https://asl-back.herokuapp.com"
-};
-
-if (window.location.hostname === "localhost") {
-  apiUrl = apiUrls.development;
-} else {
-  apiUrl = apiUrls.production;
-}
+// if (window.location.hostname === "localhost") {
+//   apiUrl = apiUrls.development;
+// } else {
+//   apiUrl = apiUrls.production;
+// }
 
 const api = axios.create({
   baseURL: apiUrl,
