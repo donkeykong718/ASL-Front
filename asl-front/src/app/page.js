033c2c8dac3f0bb3721015b11f99210dd8007c47
donkeykong718 <122040169+donkeykong718@users.noreply.github.com>
2023-04-06 16:10:44 -0400

    Well, vercel doesn't work but now it should work locally again

--- asl-front/src/app/page.js
+++ asl-front/src/app/page.js
@@ -1,11 +1,6 @@
-
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import '../app/globals.css'
 import '98.css'
-import Loading from './loading'
-import { Suspense } from 'react'
 
 
 export default function Home() {
